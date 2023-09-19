a9cd60268f9fec0f0633f73bcb8cbdb55c9810ac
Nathan Clover <cloverww04@gmail.com>
2023-09-18 19:23:03 -0500

    changes to get all posts endpoint

---
+++ DTOs/PostDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Rare_Yellow_Tigers.DTOs
+{
+    public class PostDTO
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? UserName { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string? Category { get; set; }
+        public List<string>? Tags { get; set; }
+    }
+}
